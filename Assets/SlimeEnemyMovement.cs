a22a14e4b2b663f9b4e19d38b4e39b87759ef3a6
ThrasyvoulosK <69692688+ThrasyvoulosK@users.noreply.github.com>
2023-05-03 01:53:01 +0300

    Adds NavMesh, new enemy, level and Timer

---
+++ Assets/SlimeEnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SlimeEnemyMovement : MonoBehaviour
+{
+    //code from https://www.youtube.com/watch?v=atCOd4o7tG4
+
+    [SerializeField]
+    private Transform movePositionTransform;
+
+    private NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        navMeshAgent.destination = movePositionTransform.position;
+    }
+}
---
+++ Assets/TimerManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimerManager : MonoBehaviour
+{
+    public static float time = 0;
+    public TMP_Text timeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeText = GetComponentInChildren<TMP_Text>();
+
+        DontDestroyOnLoad(this.gameObject);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //based on code from https://gamedevbeginner.com/how-to-make-countdown-timer-in-unity-minutes-seconds/
+        time += Time.deltaTime;
+
+        float minutes = Mathf.FloorToInt(time / 60);
+        float seconds = Mathf.FloorToInt(time % 60);
+        float decSeconds = time % 1*100;
+
+        timeText.text = string.Format("{0:00}:{1:00}:{2:00}",minutes,seconds,decSeconds);
+    }
+}
