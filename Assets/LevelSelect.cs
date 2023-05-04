7f29450b6ca26405e8d31508db271234e96c27ef
ThrasyvoulosK <69692688+ThrasyvoulosK@users.noreply.github.com>
2023-05-04 03:06:06 +0300

    Disables unreached levels in level select

--- Assets/LevelSelect.cs
+++ Assets/LevelSelect.cs
@@ -19,36 +19,34 @@ public class LevelSelect : MonoBehaviour
         int totalLevels = gameObject.transform.childCount;
         Debug.Log(totalLevels);
         int i = 1;
-        int tempNum;
         for ( i=1;i<=totalLevels; i++)
         {
-            Debug.Log(i);
-            Debug.Log(gameObject.transform.GetChild(i - 1));
-            tempNum = i;
-            gameObject.transform.GetChild(i-1).GetComponent<Button>().onClick.AddListener(delegate
+            Button currentButton = gameObject.transform.GetChild(i - 1).GetComponent<Button>();
+            currentButton.onClick.AddListener(delegate
             {
-                //Debug.Log("Move to scene " + i);
-                //Debug.Log(i == 4);
-                //int childNum = i;
-                //StartCoroutine(crossFade.MoveToScene(i));
-                //StartCoroutine(crossFade.MoveToScene(1));
-                //StartCoroutine(crossFade.MoveToScene(SceneManager.GetActiveScene().buildIndex+i));
-                //int tempNum
+                
                 int j = 0;
                 for ( j=0; j<gameObject.transform.childCount; j++)
                 {
-                    Debug.Log(j);
-                    Debug.Log(gameObject.transform.GetChild(j).name);
+                    //Debug.Log(j);
+                    //Debug.Log(gameObject.transform.GetChild(j).name);
                     if (gameObject.transform.GetChild(j).name == EventSystem.current.currentSelectedGameObject.name)
                         break;
                 }
-                Debug.Log(EventSystem.current.currentSelectedGameObject.name);
-                //StartCoroutine(crossFade.MoveToScene(tempNum));
+                //Debug.Log(EventSystem.current.currentSelectedGameObject.name);
+
                 StartCoroutine(crossFade.MoveToScene(j+1));
 
             });
+
+            //disable level(s) beyond the current one
+            //Debug.Log("Current level is "+SavedValues.CURRENT_LEVEL);
+            if (SavedValues.CURRENT_LEVEL <= (i - 1))
+                currentButton.interactable = false;
+
+
         }
-        Debug.Log(i);
+        
     }
 
     // Update is called once per frame
